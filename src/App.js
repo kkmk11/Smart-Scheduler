2e4f706d7676d74584ef3c1c5f23349dbe47165a
Mahesh Kumar <88311960+kkmk11@users.noreply.github.com>
2023-01-22 17:10:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,3 @@
-// import React,{createContext,useState} from 'react'
-// import ComponentA from './ComponentA'
-// import ComponentB from './ComponentB'
-
-// export const store=createContext();
-
-// const App = () => {
-//   const [data1,setData1]=useState(0);
-//   const [data2,setData2]=useState(100);
-//   return (
-//     <div>
-//       <center>
-//       <store.Provider value={[data1,setData1]}>
-//         <ComponentA/>
-//         <button className='btn btn-primary' onClick={()=>setData1(1+data1)}>Increment</button>
-//       </store.Provider><br/><br/>
-//       <store.Provider value={[data2,setData2]}>
-//         <ComponentB/>
-//         <button className='btn btn-danger' onClick={()=>setData2(data2-1)}>Decrement</button>
-//       </store.Provider><br/><br/>
-//       <div className='col-sm-3 card'>
-//         <div className='card-body'>
-//             <h3>Total sum is : {data1+data2}</h3>
-//         </div>
-//       </div>
-//       </center>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
 import React,{createContext,useState} from 'react'
 import Count from './Count'
 import Todolist from './Todolist';
